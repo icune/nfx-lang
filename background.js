fbc0ad098a449db88af3a8ee52510be8dc4a9eb6
icune <icune@ya.ru>
2022-11-06 18:49:46 +0400

    feat: added catching netflix subs

--- background.js
+++ background.js
@@ -1,7 +1,23 @@
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = 'red';
+    // document.body.style.backgroundColor = 'red';
     console.log("Hello");
-    alert("XXX");
+    let el = document.querySelector('[data-uia="player"]');
+    const config = { attributes: true};
+    const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+            if (mutation.attributeName === "class") {
+                if (mutation.target.className.indexOf("active") !== -1) {
+                    let el = document.querySelector('[data-uia="control-audio-subtitle"]');
+                    if (el) {
+                        el.click();
+                    }
+                }
+            }
+        }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(el, config);
+
 }
 
 
@@ -18,7 +34,7 @@ chrome.tabs.onActivated.addListener(
         chrome.scripting.executeScript(
             {
                 target: {tabId: tab.id},
-                files: ['script.js']
+                func: changeBackgroundColor
             },
             () => {
                 console.log("Executed");
